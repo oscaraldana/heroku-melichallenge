04278f65a164f4a6dc1f15d8faad60c9311edb06
Oscar Aldana <oealdana@gmail.com>
2022-10-14 08:29:48 -0500

    first commit

---
+++ src/test/java/com/melichallenge/heroku/HerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melichallenge.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
